0334d4e08b3e267437e92c0a1d4295759bfe9778
oscar woodhouse <oscarwoodhouse17@gmail.com>
2020-10-28 16:04:45 +0100

    first commit

---
+++ src/components/SelectionList.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Ma sélection</h1>
+  <p>Vous n'avez pas de sélection de joueurs</p>
+  {{ selection }}
+</template>
+
+<script>
+export default {
+  name: "selection-list",
+  props: ['selection']
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/global/BaseButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    class="bg-white my-3 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded transition duration-200 ease-in-out">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "base-button",
+  props: ["text"],
+};
+</script>
+
+<style>
+</style>
